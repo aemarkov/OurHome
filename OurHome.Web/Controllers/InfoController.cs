f07e31446ea18bafdc3193342f38fb876938bd06
Markov Alex <MarkovAlex95@gmail.com>
2017-03-11 18:23:58 +0300

    Сделал верстку страниц полезной информации

---
+++ OurHome.Web/Controllers/InfoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurHome.Web.Controllers
+{
+    /// <summary>
+    /// Полезная информация
+    /// </summary>
+    [AllowAnonymous]
+    public class InfoController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Post(int id)
+        {
+            return View();
+        }
+    }
+}
